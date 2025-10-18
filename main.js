d8fe00900c40f5abd1fcf22f8a7b9591eb7c1ec0
Mina Gamil Tawfik <70597499+mina442@users.noreply.github.com>
2025-10-18 11:56:15 +0300

    JSBootcamp 010_ 017

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log(elzero.innerHTML); // object
+ console.log(typeof elzero); // object
+let NumberOne = 10;
+let NumberTwo = 20;
+document.write
+console.log(`I'm In \
+\\\
+Love \\ """ '''\
+++ With ++\
+\"""\"""\
+\"\"JavaScript\"\"\``);
+
+console.log('`I\'m In \n \\\\ \n Love \\\\ \"\"\" \'\'\' \n ++ with ++ \\ \"\"\" \\ \"\"\" \n""javascript"" \``')
