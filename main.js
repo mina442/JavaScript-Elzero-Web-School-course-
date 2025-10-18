66138b5326813a6b7d06ee41915c3bc463e166dd
Mina Gamil Tawfik <70597499+mina442@users.noreply.github.com>
2025-10-18 12:33:31 +0300

    تكليفات JavaScript Bootcamp من الدرس 010 إلى 017

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
-console.log(elzero.innerHTML); // object
- console.log(typeof elzero); // object
+// console.log(elzero.innerHTML); // object
+//  console.log(typeof elzero); // object
+// Add Variables Here
 let NumberOne = 10;
 let NumberTwo = 20;
-document.write
+let h = NumberOne + "" + NumberTwo;
+
+// Ouput
+console.log(NumberOne+""+NumberTwo); // Normal Concatenate => 1020
+console.log(typeof (NumberOne+''+NumberTwo) ); // Normal Concatenate => String
+console.log(`${NumberOne}${NumberTwo}`); // Template Literals Way => 1020
+console.log(typeof`${NumberOne}${NumberTwo}`);  // Template Literals Way => String
+
+console.log(NumberTwo+"\n"+NumberOne);
+/*
+  Normal Concatenate
+  20
+  10
+*/
+
+console.log(`${NumberOne}
+${NumberTwo}`);
+/*
+  Template Literals Way
+  20
+  10
+*/
+let a = 21;
+let b = 20;
+let pair = `${b}${a}_`;
+let result = pair.repeat(3) ;
+console.log(`_${a}_${result}_${b}_`); // _21_2021_2021_2021_20_
 console.log(`I'm In \
 \\\
 Love \\ """ '''\
