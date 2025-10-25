f569d7ca3fbd62647a63e270c3c8f029a07cf537
Mina Gamil Tawfik <70597499+mina442@users.noreply.github.com>
2025-10-25 13:29:42 +0300

    تكليفات JavaScript Bootcamp من الدرس 010 إلى 017 .3  Variable And Concatenation Challenge

--- main.js
+++ main.js
@@ -1,45 +1,60 @@
-// console.log(elzero.innerHTML); // object
-//  console.log(typeof elzero); // object
-// Add Variables Here
-let NumberOne = 10;
-let NumberTwo = 20;
-let h = NumberOne + "" + NumberTwo;
+// ===========================================
+// == Variables And Concatenation Challenge ==
+// ===========================================
 
-// Ouput
-console.log(NumberOne+""+NumberTwo); // Normal Concatenate => 1020
-console.log(typeof (NumberOne+''+NumberTwo) ); // Normal Concatenate => String
-console.log(`${NumberOne}${NumberTwo}`); // Template Literals Way => 1020
-console.log(typeof`${NumberOne}${NumberTwo}`);  // Template Literals Way => String
+// [1] Create 3 Variables [Title, Desctiption, Date]
+// -- All In One Statement
+// -- Variable Name Must Be Two Words
+// -- Title Content Is "Elzero"
+// -- Description Content Is "Elzero Web School"
+// -- Date Content Is "25/10"
+let Title_Content = "Elzero",
+Desctiption_Content = "Elzero Web School", 
+    Date_Content = "25/10";
+// [2] Create Variable Contains Div And This Div Contains
+//تجربه رقم ٢
+// let card = document.createElement("div");
+// card.classList.add("card");
 
-console.log(NumberTwo+"\n"+NumberOne);
-/*
-  Normal Concatenate
-  20
-  10
-*/
+// card.innerHTML = `
+//   <h3>${Title_Content}</h3>
+//         <p>${Desctiption_Content}</p>
+//         <span>${Date_Content}</span>
+// `;
+// document.body.appendChild(card);
+// [1] إنشاء 3 متغيرات في سطر واحد
+let titleName = "Elzero",
+descContent = "Elzero Web School",
+dateValue = "25/10";
 
-console.log(`${NumberOne}
-${NumberTwo}`);
-/*
-  Template Literals Way
-  20
-  10
-*/
-let a = 21;
-let b = 20;
-let pair = `${b}${a}_`;
-let result = pair.repeat(3) ;
-console.log(`_${a}_${result}_${b}_`); // _21_2021_2021_2021_20_
-console.log(
-`I'm In \\\\ Love \\ """ '''\ ++ With ++\
-\"""\"""\ \"\"JavaScript\"\"\``);
-console.log(
-  'I\'m In\n' +
-  '\\\\\n' +
-  'Love \\\\ """ \'\'\'\n' +
-  '++ With ++\n' +
-  '\\"""\\""\"\n' +
-  '""JavaScript""``'
-);
-console.log('`I\'m In \n \\\\ \n Love \\\\ \"\"\" \'\'\' \n ++ with ++ \\ \"\"\" \\ \"\"\" \n""javascript"" \``');
+// [2] إنشاء كود الكارت باستخدام Template Literals
+let card = `
+<div class="card">
+<h3>${titleName}</h3>
+<p>${descContent}</p>
+<span>${dateValue}</span>
+</div>
+`;
 
+// [3] إضافة الكارت للصفحة 4 مرات باستخدام repeat()
+document.write(card.repeat(4));
+
+// تجربه رقم واحد
+// let ca1 =
+//     <div class="Card">
+//         <h3>`${Title_Content}</h3>
+//         <p>`${Desctiption_Content}</p>
+//         <span>`${Date_Content}</span>
+//     </div>;
+// document.write(
+//     `${ca1}`
+// );
+//التجربه الاوله ناجحه ولكن كان ينقصها بعض التفاصيل 
+// -- H3 For Title
+// -- P For Paragraph
+// -- Span For Time
+// [3] Add This Card To Page 4 Times
+// [4] Use Template Literals For Concatenate
+
+// Extra
+// - Use ES6 Repeat
